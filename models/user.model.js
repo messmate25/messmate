5c5327167305131787c9a28837ae67e34301aa3e
Souvik Ghosh <129908887+Souvik-Ghosh-js@users.noreply.github.com>
2025-08-26 01:49:50 +0530

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,44 +1,54 @@
 // File: models/user.model.js
 
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      room_no: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      role: {
+        type: DataTypes.ENUM("student", "admin", "super_admin", "external"),
+        allowNull: false,
+        defaultValue: "student",
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    room_no: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    role: {
-      type: DataTypes.ENUM('student', 'admin', 'super_admin'),
-      allowNull: false,
-      defaultValue: 'student'
-    },
-  }, {
-    tableName: 'users',
-    timestamps: true
-  });
+    {
+      tableName: "users",
+      timestamps: true,
+
+      indexes: [
+        {
+          unique: true,
+          fields: ["email"],
+        },
+      ],
+    }
+  );
 
   return User;
 };
